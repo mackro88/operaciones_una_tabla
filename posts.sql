a6997644bbb0f8a5fd5550b2148107995a8cfb2c
Romina Pineda <romi.macka@gmail.com>
2020-09-25 18:05:03 -0300

    Add column titulo

--- posts.sql
+++ posts.sql
@@ -2,7 +2,7 @@
 -- Crear base de datos "posts"
 CREATE DATABASE posts;
 \c posts;
--- Crear tabla "post"
+-- Crear tabla post, con los atributos id, nombre de usuario, fecha de creación, contenido, descripción.
 CREATE TABLE post(
     id SERIAL,
     nombre_de_usuario VARCHAR(30),
@@ -10,6 +10,7 @@ CREATE TABLE post(
     contenido TEXT,
     descripcion VARCHAR(250)
 );
+-- Insertar 3 post, 2 para el usuario "Pamela" y uno para "Carlos"
 INSERT INTO post(
     nombre_de_usuario,
     fecha_de_creacion,
@@ -28,3 +29,6 @@ VALUES
     '2020/07/04',
     'Los quetzales (Pharomachrus) son un género de aves de colores muy llamativos y vistosos, habitantes de los bosques y las selvas de Centroamérica y de América del Sur, y que gozaron de una gran importancia en la cultura de las civilizaciones prehispánicas. En este sentido, los quetzales fueron considerados como aves sagradas, y sus plumas eran utilizadas por estas antiguos culturas para la fabricación de los ornamentos de los sacerdotes del dios Quetzalcóatl, o la Serpiente Emplumada. Las plumas de la cola de los machos pueden alcanzar casi un metro de longitud, siendo considerablemente más largas que el propio cuerpo.',
     'Las plumas de algunas aves sagradas y exóticas.');
+-- Modificar la tabla post, agregando la columna título.
+ALTER TABLE post ADD titulo VARCHAR(50);
+-- Agregar título a las publicaciones ya ingresadas
